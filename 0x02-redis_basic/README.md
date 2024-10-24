9774ca0facd2da3ae92ab53af60c6cbfd94e5604
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-24 13:36:29 +0000

    Generating random number and a string

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,3 @@
+Task 1#
+	It Implements the Cache class with required functionality for storing data in REdis using a randomly generated key.
+ 
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env pythin3
+"""
+This module provides a Cache class that interacts with a Redis db.
+"""
+
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+    CAche class for storing and retrieving data
+    """
+
+    def __init__(self):
+        """
+        Initialize the Redis client and flush
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Storage the input data in redis using a random key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
