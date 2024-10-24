0b86e6d40a8fc6dc655fab0752bd7ce3e34e8946
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-24 14:17:36 +0000

    reading from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ This module provides a Cache class that interacts with a Redis db.
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -26,3 +26,28 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self,
+        key: str,
+        fn: Optional[Callable[[bytes], Union[str, int, float, bytes]]] = None,
+    ) -> Union[str, int, float, bytes, None]:
+        """
+        Retrieve the data stored
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve data as a UTF-8 decode
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve data as an integer
+        """
+        return self.get(key, fn=int)
