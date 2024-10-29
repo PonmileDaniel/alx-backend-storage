9ed49a52c617d12d314ed0de2817679d8106024a
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-29 16:26:57 +0000

    Top score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Top
+"""
+
+
+def top_students(mongo_collection):
+    """
+    return all student
+    """
+    return mongo_collection.aggregate(
+        [
+            {"$project": {"name": "$name", "averageScore": {"$avg": "topics.score"}}},
+            {"$sort": {"averageScore": -1}},
+        ]
+    )
