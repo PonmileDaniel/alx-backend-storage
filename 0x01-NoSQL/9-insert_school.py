380839e5ee22abf3a6c4e2ff871a3985ff31510d
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-29 15:34:02 +0000

    Insert school

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Insert a doc 
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    INsert a new doc
+    """
+    doc = mongo_collection.insert_one(kwargs)
+    return doc.insert_id
